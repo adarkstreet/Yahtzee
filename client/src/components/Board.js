1ebb2d70f51a3ad3fffceb7cc03142b457004784
Andrew Street <andrew_street@live.com>
2018-03-26 11:54:28 -0600

    update

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { Grid, Button, Divider } from 'semantic-ui-react'
+import Dice from './Dice'
 
-const Board = () => <div>Board</div>
+const Board = ({ roll, dice, rollDice }) => {
+  const maxRoll = roll === 3;
+  const disabled = maxRoll ? { disabled: true } : {}
+  return (
+    <Grid>
+      <Grid.Row>
+        <Button
+          fluid
+          onClick={rollDice}
+          {...disabled}
+        >
+          { maxRoll ? 'Score Roll' : 'Roll Dice' }
+        </Button>
+        <Grid.Column width={16}>
+          <Divider hidden />
+        </Grid.Column>
+        { roll > 0 &&
+            dice.map( (d,i) => <Dice key={i} value={d} /> )
+        }
+      </Grid.Row>
+    </Grid>
+  )
+}
 
-export default Board;
+export default Board
