6bac53819c1b7dcb0e7cd5aa9b94bfdf4b44a6ef
Andrew Street <andrew_street@live.com>
2018-03-27 10:57:41 -0600

    refactoring

---
+++ client/src/reducers/currentGame.js
@@ -0,0 +1,20 @@
+import { TOGGLE_KEPT, ROLL_DICE } from '../actions/currentGame';
+
+const currentGame = (
+  state = { roll: 0, dice: [...new Array(5)], keep: [], }, action
+) => {
+  switch (action.type) {
+    case ROLL_DICE:
+      return {
+        ...state, dice: action.dice, roll: state.roll + 1
+      }
+    case TOGGLE_KEPT:
+      return {
+        ...state, keep: action.keep
+      }
+    default:
+      return state;
+  }
+}
+
+export default currentGame;
